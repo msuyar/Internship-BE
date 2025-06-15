ebf03b80fe1c26834c168e828c4ccde65b1d5fe0
msuyar <saidx35@gmail.com>
2025-06-15 14:18:54 +0300

    created a function for the User to get all his reviews

---
+++ LMS.Data/DTO's/MovieDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.Data.Dtos;
+
+public class MovieDetailsDto
+{
+    public string Title { get; set; }
+    public double AverageRating { get; set; }
+    public List<ReviewDto> ReviewDtos { get; set; }
+    
+}
