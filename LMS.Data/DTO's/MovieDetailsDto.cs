c29c6991f4c272f3ce5054c2b89188e2997bfc22
msuyar <saidx35@gmail.com>
2025-06-16 12:24:49 +0300

    Created a GetMovieDetails function

--- LMS.Data/DTO's/MovieDetailsDto.cs
+++ LMS.Data/DTO's/MovieDetailsDto.cs
@@ -2,7 +2,14 @@
 
 public class MovieDetailsDto
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
+    public string Plot { get; set; }
+    public string Cast { get; set; }
+    public string Director { get; set; }
+    public string Category { get; set; }
+    public int Duration { get; set; }
+    public DateTime ReleaseDate { get; set; }
     public double AverageRating { get; set; }
     public List<ReviewDto> ReviewDtos { get; set; }
     
